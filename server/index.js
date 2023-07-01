90942026307bc1ec07a35626349780fdab06ac39
Divya Kulkarni <103066357+divya-kulkarni99@users.noreply.github.com>
2023-06-30 18:24:55 -0700

    Update index.js

--- server/index.js
+++ server/index.js
@@ -14,6 +14,10 @@ connection();
 app.use(express.json());
 app.use(cors());
 
+app.get("/",(req,res) =?{
+  res.json("Hello");
+  }
+)
 // routes
 app.use("/api/users", userRoutes);
 app.use("/api/auth", authRoutes);
