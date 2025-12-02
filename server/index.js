4b2d5052570284894e6342210027501801fbe258
Divya Kulkarni <divyasuhaskulkarni@gmail.com>
2025-12-01 19:08:47 -0800

    Resolved cors

--- server/index.js
+++ server/index.js
@@ -1,33 +1,34 @@
-require("dotenv").config();
-const express = require("express");
+require('dotenv').config();
+const express = require('express');
 const app = express();
-const cors = require("cors");
-const connection = require('./userinformation')
+const cors = require('cors');
+const connection = require('./userinformation');
 const userRoutes = require('./routes/users');
 const authRoutes = require('./routes/auth');
 
-
 // database connection
-connection().catch(err => console.error("Database connection error:", err));
+connection().catch((err) => console.error('Database connection error:', err));
 
 // middlewares
 app.use(express.json());
-app.use(cors(
-  {
-      origin: ["https://nasa-apodapi-webapp-1.vercel.app", "https://nasa-apodapi-webapp-lyart.vercel.app", "http://localhost:3000"],
-      methods: ["POST", "GET"],
-      credentials: true
-  }
-));
+app.use(
+  cors({
+    origin: [
+      'https://nasa-apodapi-webapp-1.vercel.app',
+      'https://nasa-apodapi-webapp-prww.vercel.app/',
+      'http://localhost:3000',
+    ],
+    methods: ['POST', 'GET'],
+    credentials: true,
+  })
+);
 
-app.get("/",(req,res) =>{
-  res.json("Hello");
-  }
-)
+app.get('/', (req, res) => {
+  res.json('Hello');
+});
 // routes
-app.use("/api/users", userRoutes);
-app.use("/api/auth", authRoutes);
+app.use('/api/users', userRoutes);
+app.use('/api/auth', authRoutes);
 
 const port = process.env.PORT || 8080;
 app.listen(port, console.log(`Listening on port ${port}...`));
-
